20061c70c56c266dc35fb287dc3c054ad4864b47
Author Name <email@address.com>
2019-04-24 22:26:21 -0400

    Fix

--- rel-site-dev.sh
+++ rel-site-dev.sh
@@ -4,6 +4,7 @@ cd priv/corto-site/
 rm -rf build/
 PUBLIC_URL=./ yarn build
 cd -
+rm -rf _build/dev/rel/corto/priv/ _build/dev/rel/corto/priv/corto-site
 mkdir _build/dev/rel/corto/priv/
 mkdir _build/dev/rel/corto/priv/corto-site
 mv priv/corto-site/build/ _build/dev/rel/corto/priv/corto-site/
